b16455677f619282062579dd326f21b320d805b2
Jenny Hoelzer <hoelzer.jenny@gmail.com>
2018-11-12 13:34:10 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const githubURL = "https://api.github.com/users/jhoelzer";
+
 class App extends Component {
+  state = {
+    user: {},
+    active: false
+  }
+
+  handleClickButton = event => {
+    console.log("clicked");
+    fetch(githubURL)
+      .then(res => res.json())
+      .then(gitInfo => {
+        this.setState({ user: gitInfo });
+        console.log({ gitInfo });
+      })
+      .catch(err => console.log(`${err} error error error`));
+    this.setState(prevState => ({
+      active: !prevState.active
+    }));
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button className="button" onClick={this.handleClickButton}>Click Here</button>
+        <div>
+
+          {this.state.active ?
+
+            <div>
+              <img src={this.state.user.avatar_url} />
+              <h3>Name: {this.state.user.name}</h3>
+              <h3>Username: {this.state.user.login}</h3>
+              <h3>Portfolio URL: 
+                <a href={this.state.user.blog} target="_blank">{this.state.user.blog}</a>
+              </h3>
+              <h3>Location: {this.state.user.location}</h3>
+            </div>
+            
+          : null}
+
+        </div>
       </div>
     );
   }
