08ae6b65110fb54d98eb09ae03d52ffee23d8774
Jenny Hoelzer <hoelzer.jenny@gmail.com>
2018-11-12 13:36:57 -0500

    Updates

---
+++ precache-manifest.2911d06ed8e11c1dbe1e24437c82d9ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17bf4564c6f97911fa43",
+    "url": "/github-cards/static/js/runtime~main.17bf4564.js"
+  },
+  {
+    "revision": "5671a3f775a30b476aa4",
+    "url": "/github-cards/static/js/main.5671a3f7.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/github-cards/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "5671a3f775a30b476aa4",
+    "url": "/github-cards/static/css/main.e3ebc5fe.chunk.css"
+  },
+  {
+    "revision": "34ef5b4121e58090aaed058f7bf023c4",
+    "url": "/github-cards/index.html"
+  }
+];
